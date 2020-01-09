ff735aa60d1b0d51967d0630952aba94bfb4e1ca
Hanan Kwok <hanankwok532@gmail.com>
2020-01-09 11:41:39 -0500

    Reorganized some files.

---
+++ Python Files/ModelTest.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  8 15:41:51 2020
+
+@author: hanan
+"""
+
+import pandas as pd # import pandas in order to read Excel files & make DataFrames
+
+nat1997 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_1997_dl.xls", sheet_name = "oes_national_1997")
+nat1998 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_1998_dl.xls", sheet_name = "oesnat98")
+nat1999 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_1999_dl.xls", sheet_name = "download_1999_nat_estimates_spr")
+nat2000 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_2000_dl.xls", sheet_name = "oesnat2000")
+nat2001 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_2001_dl.xls", sheet_name = "nat_int_updated")
+nat2002 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_2002_dl.xls", sheet_name = "national")
+natMay2003 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2003_dl.xls", sheet_name = "national_dl")
+natNov2003 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_november2003_dl.xls", sheet_name = "national_dl")
+natMay2004 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2004_dl.xls", sheet_name = "national_dl")
+natNov2004 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_november2004_dl.xls", sheet_name = "national_dl")
+natMay2005 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2005_dl.xls", sheet_name = "national_dl")
+natMay2006 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2006_dl.xls", sheet_name = "national_dl")
+natMay2007 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2007_dl.xls", sheet_name = "national_dl")
+natMay2008 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2008_dl.xls", sheet_name = "national_dl")
+natMay2009 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2009_dl.xls", sheet_name = "national_dl")
+natMay2010 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2010_dl.xls", sheet_name = "national_dl")
+natMay2011 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2011_dl.xls", sheet_name = "national_dl")
+natMay2012 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2012_dl.xls", sheet_name = "national_dl")
+natMay2013 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2013_dl.xls", sheet_name = "national_dl")
+natMay2014 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2014_dl.xlsx", sheet_name = "national_dl")
+natMay2015 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2015_dl.xlsx", sheet_name = "national_dl")
+natMay2016 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2016_dl.xlsx", sheet_name = "national_dl")
+natMay2017 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2017_dl.xlsx", sheet_name = "national_dl")
+natMay2018 = pd.read_excel("C:/Users/hanan/Desktop/PersonalProject/Data/national_may2018_dl.xlsx", sheet_name = "national_dl")
