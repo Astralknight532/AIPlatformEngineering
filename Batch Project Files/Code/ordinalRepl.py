ee23781166ea16d832f6311460f09904cd28afa6
Hanan Kwok <hanankwok532@gmail.com>
2020-01-17 09:40:18 -0500

    Updated Batch Project files

--- Batch Project Files/Code/ordinalRepl.py
+++ Batch Project Files/Code/ordinalRepl.py
@@ -4,8 +4,7 @@
 Created on Mon Jan 13 16:11:09 2020
 @author: Matt Davis, Ragy, Alex, Stephen
  Takes series / dict / int 
- Fill na's with recieved int valuead('pcaObject.joblib')
-np_final = pcaobj
+ Fill na's with recieved int value
  return series with updated dictionary
  """
 
