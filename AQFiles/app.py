ae0cc6027c8a855d24e9b9a5c428440552a3c826
Astralknight532 <hanankwok532@gmail.com>
2020-02-17 09:05:22 -0500

    Uploaded starter files for a Flask app deployment

---
+++ AQFiles/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def basic_route():
+    """Render the base template"""
+    return render_template("index.html")
+
+@app.route("/special")
+def special_route():
+    """Render an extended version of the base template"""
+    return render_template("results.html")
+
+app.run(debug = True)
