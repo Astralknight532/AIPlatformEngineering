f4814ea60f838c1d3d2be2bfd2f977a5ade9885b
Astralknight532 <hanankwok532@gmail.com>
2020-02-23 11:52:51 -0500

    Fixed errors in prediction code on Flask deployment

--- AQFiles/app.py
+++ AQFiles/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 # Loading the models
 # NO2 model
-no2model = load_model('C:/Users/hanan/Desktop/PersonalRepository/AQFiles/SavedModels/no2_model.h5') 
+no2_model = load_model('C:/Users/hanan/Desktop/PersonalRepository/AQFiles/SavedModels/no2_model.h5') 
 # SO2 model
 so2_model = load_model('C:/Users/hanan/Desktop/PersonalRepository/AQFiles/SavedModels/so2_model.h5') 
 # O3 model
@@ -28,21 +28,23 @@ def home_page():
 def predict_result():
     date = request.form["dateentry"] # Get the date entered by the user
     pol = request.form["polselect"] # Get the pollutant chosen by the user 
-    avgconc = round(random.uniform(0, 100), 3) # Create a variable to store the predicted avg. concentration for a pollutant
-    #date = pd.to_datetime(date, format = '%Y-%m-%d')
-    #pred_input = np.array([date - timedelta(days = 2), date - timedelta(days = 1)])
-    #pred_input = np.array(pd.DataFrame(pred_input)).reshape((1, 2, 1))
+    avgconc = 0 # Create a variable to store the predicted avg. concentration for a pollutant
+    date = pd.to_datetime(date, format = '%Y-%m-%d')
+    pred_input = np.array([date - timedelta(days = 2), date - timedelta(days = 1)])
+    pred_input = np.array(pd.DataFrame(pred_input)).reshape((1, 2, 1))
     # Select the appropriate model based on the user's chosen pollutant
     if pol == 'NO2':
         print('NO2 Model')
-        #no2pred = no2model.predict(pred_input)
-        #avgconc = round(no2pred[0][0], 3)
+        avgconc = no2_model.predict(pred_input)[0][0]
     elif pol == 'SO2':
         print('SO2 Model')
+        avgconc = so2_model.predict(pred_input)[0][0]
     elif pol == 'O3':
         print('O3 Model')
+        avgconc = o3_model.predict(pred_input)[0][0]
     elif pol == 'CO':
         print('CO Model')
+        avgconc = co_model.predict(pred_input)[0][0]
 
     avgconc_print = str(avgconc)
     if pol == 'NO2' or pol == 'SO2':
@@ -50,6 +52,6 @@ def predict_result():
     elif pol == 'O3' or pol == 'CO':
         avgconc_print += ' parts per million'
     
-    return render_template("results.html", chosendate = str(date), pollutant = pol, avgconc = avgconc_print)
+    return render_template("results.html", chosendate = str(date.strftime('%Y-%m-%d')), pollutant = pol, avgconc = avgconc_print)
 
-app.run(debug = True)
+app.run(host = '127.0.0.1', port = '5000', debug = False, threaded = False)
